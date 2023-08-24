69f14e267c7a2f4b1351381468ba5080b61457f6
IsaiasMella <isaias.mella2013@gmail.com>
2023-08-24 15:30:36 -0300

    add component footer

---
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <div className="absolute bottom-0 left-0 w-full">
+        <ul className="cursor-default flex w-full justify-center pb-7 gap-x-4 text-[13px]">
+            <li>Tesla © 2023</li>
+            <li>Privacidad y legal</li>
+            <li>Contacto</li>
+            <li>Noticias</li>
+            <li>Seguir informado</li>
+            <li>Localización de tiendas</li>
+        </ul>
+    </div>
+  )
+}
