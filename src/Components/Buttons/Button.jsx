954eef3dddc7d70767156523964a53ec384fe211
IsaiasMella <isaias.mella2013@gmail.com>
2023-08-24 16:57:30 -0300

    added animation for home page

--- src/Components/Buttons/Button.jsx
+++ src/Components/Buttons/Button.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const typeButton = {
   outline:
     "text-white cursor-pointer rounded-[3px] font-medium text-sm border-white border-[3px] py-[9px] px-12 hover:bg-white hover:text-black",
@@ -7,14 +9,23 @@ const typeButton = {
     "cursor-pointer bg-white text-sm rounded font-medium px-14 py-2 transition-colors hover:bg-black hover:text-white inline-flex justify-center items-center text-black",
 };
 
+const animationButton = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1,
+      duration: 0.5,
+    },
+  },
+};
+
 export const Button = ({ type, title, customStyle = "" }) => {
   return (
-    <a
-      className={
-        `cursor-pointer ` + typeButton[type] + customStyle
-      }
-    >
-      {title}
-    </a>
+    <motion.div variants={animationButton} initial="hidden" animate="visible">
+      <a className={`cursor-pointer ` + typeButton[type] + customStyle}>
+        {title}
+      </a>
+    </motion.div>
   );
 };
--- src/Components/Layouts/HomeLayouts/MainHomeLayout.jsx
+++ src/Components/Layouts/HomeLayouts/MainHomeLayout.jsx
@@ -1,15 +1,55 @@
-export const MainHomeLayout = ({ children, title, subtitle, setColor,textColor }) => {
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { duration: .3 },
+    },
+  },
+};
+
+export const MainHomeLayout = ({
+  children,
+  title,
+  subtitle,
+  setColor,
+  textColor,
+}) => {
   return (
     <section className="h-full w-full relative">
-      <header id="nav-bar" className="h-screen w-full flex flex-col items-center justify-between">
-        <div
+      <header
+        id="nav-bar"
+        className="h-screen w-full flex flex-col items-center justify-between"
+      >
+        <motion.div
           id="section-home"
           data-nav-color={setColor}
+          variants={container}
+          initial="hidden"
+          animate="visible"
           className="pt-[5.5rem] text-center z-30"
         >
-          <h1 className={textColor}>{title}</h1>
-          <p className={textColor + ' text-sm'}>{subtitle}</p>
-        </div>
+          <motion.h1 variants={item} className={textColor}>
+            {title}
+          </motion.h1>
+          <motion.p variants={item} className={textColor + " text-sm"}>
+            {subtitle}
+          </motion.p>
+        </motion.div>
         {children}
       </header>
     </section>
