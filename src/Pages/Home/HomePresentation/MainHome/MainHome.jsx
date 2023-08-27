0848b19f280bdf85c99ee9994eef4f9bd895ebdb
IsaiasMella <67384494+IsaiasMella@users.noreply.github.com>
2023-08-27 15:39:45 -0300

    Update MainHome.jsx

--- src/Pages/Home/HomePresentation/MainHome/MainHome.jsx
+++ src/Pages/Home/HomePresentation/MainHome/MainHome.jsx
@@ -11,7 +11,7 @@ export const MainHome = () => {
           subtitle="Concertar una prueba de conducción"
           setColor="#000"
         >
-          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-8 md:mb-0">
+          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-10 md:mb-0">
             <Button type="darkFill" title="Explorar inventario" />
             <Button type="lightFill" title="Pedido personalizado" />
           </div>
@@ -25,7 +25,7 @@ export const MainHome = () => {
           subtitle="Concertar una prueba de conducción"
           setColor="#000"
         >
-          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-8 md:mb-0">
+          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-10 md:mb-0">
             <Button type="darkFill" title="Explorar inventario" />
             <Button type="lightFill" title="Pedido personalizado" />
           </div>
@@ -39,7 +39,7 @@ export const MainHome = () => {
           subtitle="Programe una consulta virtual"
           setColor="#000"
         >
-          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-8 md:mb-0">
+          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-10 md:mb-0">
             <Button type="darkFill" title="Pedidoper sonalizado" />
             <Button
               customStyle="px-[8.2rem] md:px-[5.63rem] "
@@ -57,7 +57,7 @@ export const MainHome = () => {
           subtitle="Programe una consulta virtual"
           setColor="#000"
         >
-          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-8 md:mb-0">
+          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-10 md:mb-0">
             <Button type="darkFill" title="Pedidoper sonalizado" />
             <Button
               customStyle="px-[8.2rem] md:px-[5.63rem]"
@@ -75,7 +75,7 @@ export const MainHome = () => {
           subtitle="Energía para todos"
           setColor="#000"
         >
-          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-8 md:mb-0">
+          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-10 md:mb-0">
             <Button
               customStyle="cursor-pointer border-[3px] border-[#393c41] bg-[#393c41] text-sm rounded font-medium text-white px-[8.2rem] md:px-[5.63rem] py-2 inline-block transition-colors hover:border-gray-500 hover:bg-gray-500"
               title="Saber más"
@@ -87,7 +87,7 @@ export const MainHome = () => {
 
       <div id="Carga" className="snap-start snap-mandatory">
         <MainHomeLayout title="Accesorios" setColor="#000">
-          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-8 md:mb-0">
+          <div className=" flex flex-col z-40 pb-[48px] md:pb-[70px] gap-y-8 md:flex-row md:gap-x-6 mb-10 md:mb-0">
             <Button
               customStyle="cursor-pointer border-[3px] border-black bg-black text-sm rounded font-medium text-white px-28 md:px-[5.63rem] py-2 inline-block transition-colors duration-300 hover:border-gray-800 hover:bg-gray-800"
               title="Comprar ahora"
